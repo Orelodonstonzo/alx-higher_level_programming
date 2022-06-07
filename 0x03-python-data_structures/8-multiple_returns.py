1acb206f6e03e153b61510536f4544de4a4e31d5
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-06-07 00:50:01 +0000

    thirty78910

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    count = len(sentence)
+    if sentence == " ":
+        first_alph = "None"
+    else:
+        first_alph = sentence[0]
+    return ((count, first_alph))
