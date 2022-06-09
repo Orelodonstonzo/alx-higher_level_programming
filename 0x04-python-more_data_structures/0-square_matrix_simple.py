8d79754c5763201c4943ce14c6c71b4b045ccdf3
Orelodonstonzo <stanleyohaji92@gmail.com>
2022-06-09 02:48:14 +0000

    corrections on 0712

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix = []):
+def square_matrix_simple(matrix=[]):
     if not matrix:
         print()
     return [[elm ** 2 for elm in row] for row in matrix]
